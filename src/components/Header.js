945b0a5d129723b627342b44ed240be27d8c675d
Ashish Kumar Singh <ashishsinghk2020@gmail.com>
2023-06-10 00:21:28 +0530

    style: added header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,27 @@
+
+
 const Header = ()=>{
     return (
-        <div className="header">
+        <div className="header flex justify-between">
             <div className="logo-container">
             <img
-                className="logo"
+                className="logo w-[140px]"
                 src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
             />
             </div>
             <div className="nav-items">
-            <ul>
-                <li className="text-4xl">Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-                <li>Cart</li>
-            </ul>
+                <ul className="flex py-6">
+                    <li className="nav-list px-5 cursor-pointer">Home</li>
+                    <li className="nav-list px-5 cursor-pointer" >About Us</li>
+                    <li className="nav-list px-5 cursor-pointer">Contact Us</li>
+                    <li className="nav-list px-5 cursor-pointer">Cart</li>
+                </ul>
+            </div>
+            <div>
+                <ul className="flex">
+                    <li className="px-[20px] py-[12px]"><button className="bg-[#1434A4] rounded-[35px] text-white w-[85px] h-[40px]">CART</button></li>
+                    <li className="px-[20px] py-[12px]"><button className="rounded-[35px] bg-black text-white w-[85px] h-[40px]">Login</button></li>
+                </ul>
             </div>
         </div>
     )
