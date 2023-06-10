42d301e3b7c90717cd47ff0d4151189183d50fa6
Ashish Kumar Singh <ashishsinghk2020@gmail.com>
2023-06-11 00:22:50 +0530

    fix minior issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,7 +9,7 @@ const Body = () => {
       <>
         <div className="search-container flex justify-center mt-[40px]">
             <input className="border border-black w-[400px] p-[3px] text-[17px] pl-[16px] h-[40px]" type="text" placeholder="Search For Restaurant..." />
-            <button className="ml-[40px] border  w-[100px] h-[40px] bg-blue-50" onClick={filterRestaurant}> 🔎 Search</button>
+            <button className="ml-[40px] border  w-[100px] h-[40px] bg-blue-50" > 🔎 Search</button>
         </div>
         <div className="body my-[40px] mx-[40px]">
           <div className="res-container flex flex-wrap">
